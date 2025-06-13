f33861ae7df8022db35bb56865fd11cd43c931a0
Mani-420 <tahirabdulrehman46@gmail.com>
2025-06-14 03:41:40 +0500

    Main Layout

--- nextjs_practice/src/app/layout.tsx
+++ nextjs_practice/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
+import { SessionProvider } from 'next-auth/react';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,18 +26,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div>
-          {' '}
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-          <Link href="/dashboard">Dashboard</Link>
-          <Link href="/">Home</Link>
-        </div>
-        {children}
-      </body>
+      <SessionProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div>
+            {' '}
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/dashboard">Dashboard</Link>
+            <Link href="/">Home</Link>
+          </div>
+          {children}
+        </body>
+      </SessionProvider>
     </html>
   );
 }
